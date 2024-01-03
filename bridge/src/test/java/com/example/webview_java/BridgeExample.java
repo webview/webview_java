2712d516e0978effe5b861a63d0b2357e4d19e6b
Ender <33337309+e3ndr@users.noreply.github.com>
2024-01-03 15:20:36 -0600

    Bridge: Update the example to be more useful.

--- bridge/src/test/java/com/example/webview_java/BridgeExample.java
+++ bridge/src/test/java/com/example/webview_java/BridgeExample.java
@@ -1,5 +1,8 @@
 package com.example.webview_java;
 
+import java.awt.Toolkit;
+
+import co.casterlabs.commons.async.AsyncTask;
 import dev.webview.webview_java.Webview;
 import dev.webview.webview_java.bridge.JavascriptFunction;
 import dev.webview.webview_java.bridge.JavascriptObject;
@@ -16,7 +19,19 @@ public class BridgeExample {
 
         wv.setTitle("My Webview App");
         wv.setSize(800, 600);
-        wv.loadURL("https://example.com");
+        wv.setHTML(
+            "<!DOCTYPE html>"
+                + "<html>"
+                + "<p>Nano Time: <span id='nano-time'></span></p>"
+                + "<button onclick='Test.ringBell();'>Ring Bell</button>"
+                + "<script>"
+                + "Test.__stores.svelte('nanoTime')"
+                + ".subscribe((time) => {"
+                + "document.querySelector('#nano-time').innerText = time;"
+                + "});"
+                + "</script>"
+                + "</html>"
+        );
 
         wv.run(); // Run the webview event loop, the webview is fully disposed when this returns.
         wv.close(); // Free any resources allocated.
@@ -24,12 +39,22 @@ public class BridgeExample {
 
     public static class TestObject extends JavascriptObject {
 
-        @JavascriptValue(watchForMutate = true)
-        public int variable = 123;
+        @JavascriptValue(allowSet = false, watchForMutate = true)
+        public long nanoTime = -1;
+        {
+            AsyncTask.create(() -> {
+                try {
+                    while (true) {
+                        this.nanoTime = System.nanoTime();
+                        Thread.sleep(100);
+                    }
+                } catch (InterruptedException ignored) {}
+            });
+        }
 
         @JavascriptFunction
-        public void doSomething(int number) {
-            System.out.println(number);
+        public void ringBell() {
+            Toolkit.getDefaultToolkit().beep();
         }
 
     }
