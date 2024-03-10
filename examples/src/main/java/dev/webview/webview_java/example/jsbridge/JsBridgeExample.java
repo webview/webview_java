a027fc88c2a8f45b4f3839151b51c30cd387d8cc
Ender <33337309+e3ndr@users.noreply.github.com>
2024-03-09 18:08:38 -0600

    Examples: That was small.

--- examples/src/main/java/dev/webview/webview_java/example/jsbridge/JsBridgeExample.java
+++ examples/src/main/java/dev/webview/webview_java/example/jsbridge/JsBridgeExample.java
@@ -6,28 +6,28 @@ import dev.webview.webview_java.bridge.WebviewBridge;
 public class JsBridgeExample {
 
     public static void main(String[] args) {
-        Webview wv = new Webview(true, 300, 200); // Can optionally be created with an AWT component to be painted on.
+        Webview wv = new Webview(true); // Can optionally be created with an AWT component to be painted on.
 
         wv.setTitle("My Webview App");
 
         WebviewBridge bridge = new WebviewBridge(wv);
-        // await jpp.a("a"), await jpp.b("a", "b"),   await jpp.c(1024),
-        // await jpp.count,  jpp.count=10
+        // await jpp.a("a"), await jpp.b("a", "b"), await jpp.c(1024),
+        // await jpp.count, jpp.count=10
         bridge.defineObject("jpp", new RandStr());
 
         // load a URL
         wv.loadURL("https://google.com");
-        //wv.loadURL("https://baidu.com");
-        //wv.loadURL(null);
+        // wv.loadURL("https://baidu.com");
+        // wv.loadURL(null);
 
         /*
-
+        
         Or, load raw html from a file with:
         wv.setHTML("<h1>This is a test!<h1>");
-
+        
         String htmlContent = loadContentFromFile("index.html");
         wv.setHTML(htmlContent);
-
+        
          */
 
         wv.run(); // Run the webview event loop, the webview is fully disposed when this returns.
